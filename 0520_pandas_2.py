655506f8b35b76b3584af2783f38dc4af01fc958
penny5520837-lang <penny5520837@gmail.com>
2026-05-22 19:25:19 +0800

    0520

--- 0520_pandas_2.py
+++ 0520_pandas_2.py
@@ -1,27 +1,39 @@
 import pandas as pd
 
-import pandas as pd
+products = ["Apple", "Banana", "Orange", "Mango", "Grape", "Guava"]
+prices = [30, 20, 25, 60, 45, 35]
+sales = [100, 150, 80, 60, 90, 54]
+
+data_dict = {"Product": products, "Price": prices, "Sales": sales}
+df_from_dict = pd.DataFrame(data_dict)
 
 
-data = {
-    "Product": ["Apple", " Banana", " Orange", "Mango", "Grape"],
-    "Price": [30, 20, 25, 60, 45],
-    "Sales": [100, 150, 80, 60, 90],
-}
-
-
-df = pd.DataFrame(data)
-print(df)
-
-
-data = [
-    ["Apple",30,100],
-    [" Banana",20,150],
-    [" Orange",25,80],
-    ["Mango",60,60],
-    [ "Grape", 45, 90]
-
+data_list = [
+    ["Apple", 30, 100],
+    ["Banana", 20, 150],
+    ["Orange", 25, 80],
+    ["Mango", 60, 60],
+    ["Grape", 45, 90],
+    ["Guava", 35, 54],
 ]
-df = pd.DataFrame(data, columns=["Product", "Price", "Sales"])
-print(df)
+df = pd.DataFrame(data_list, columns=["Product", "Price", "Sales"])
 
+print(df.head(5))
+print(df.tail(5))
+
+
+print(df.shape)
+
+print(df.columns)
+
+
+print(df.dtypes)
+
+
+print(df.count())
+
+
+summary_stats = df.describe().round(2)
+print(summary_stats)
+
+summary_stats.to_csv("0520_stock2.csv", index=True)
